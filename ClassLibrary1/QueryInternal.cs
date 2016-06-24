cb965d263c561525e5927d7df9852ce64e59e35b
sldgoer <13500288851@163.com>
2016-06-24 14:34:19 +0800

    Master

---
+++ ClassLibrary1/QueryInternal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace QueryFromInternal
+{
+    public class QueryInternal
+    {
+        public QueryInternal(){}
+
+        LinqToJMGJJFactDataContext ltjdc = new LinqToJMGJJFactDataContext();
+
+        public IEnumerable<lxb_EX_GDBS_SBResult> QuerySBData(DateTime begindate,DateTime enddate)
+        {
+            var bjresult = from r in ltjdc.lxb_EX_GDBS_SB(begindate, enddate)
+                           select r;
+            return bjresult;
+
+        }
+
+    }
+}
---
+++ DataTransToOnline/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QueryFromInternal;
+
+namespace DataTransToOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printdata();
+        }
+
+        static void printdata()
+        {
+            QueryInternal qi = new QueryInternal();
+            var sb = qi.QuerySBData(new DateTime(2016, 6, 1), DateTime.Now.Date);
+            foreach (var o in sb)
+            {
+                Type t=o.GetType();
+                foreach (var p in t.GetProperties())
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
